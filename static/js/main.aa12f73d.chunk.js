a40f6e4690665022047e41503746eaf54841df5c
Oleg Lozovik <oldisc3@gmail.com>
2021-09-10 23:27:27 +0300

    Updates

--- static/js/main.aa12f73d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-image-finder"]=this["webpackJsonpgoit-react-hw-03-image-finder"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var r=n(1),c=n.n(r),i=n(3),o=n.n(i),a=(n(8),n(9),n(0));var s=function(){return Object(a.jsx)(a.Fragment,{children:Object(a.jsx)("div",{children:"Some text"})})};o.a.render(Object(a.jsx)(c.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.aa12f73d.chunk.js.map
